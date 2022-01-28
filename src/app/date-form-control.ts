70e8df7ff9187a7cdfd0059f364e21c510e5f99a
airbass <artur.basinski@gmail.com>
2022-01-28 13:51:09 +0100

    Adding ngx-mask

--- src/app/date-form-control.ts
+++ src/app/date-form-control.ts
@@ -1,2 +1,34 @@
-export class DateFormControl {
+import { FormControl } from '@angular/forms';
+
+export class DateFormControl extends FormControl {
+  override setValue(
+    value: any,
+    options?: {
+      onlySelf?: boolean | undefined;
+      emitEvent?: boolean | undefined;
+      emitModelToViewChange?: boolean | undefined;
+      emitViewToModelChange?: boolean | undefined;
+    }
+  ): void {
+    if (value.match(/[^0-9|\/]/gi)) {
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+    if (value.length > 5) {
+      super.setValue(this.value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+    if (value.length === 2 && this.value.length === 3) {
+      super.setValue(value, { ...options, emitModelToViewChange: true });
+      return;
+    }
+
+    if (value.length === 2) {
+      super.setValue(value + '/', { ...options, emitModelToViewChange: true });
+      return;
+    }
+    super.setValue(value, { ...options, emitModelToViewChange: true });
+  }
 }
+
+// value is just function argument and this.value is property of FormControl class instance
